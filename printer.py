0be565bd8cb62145ea3d82c762ca83fbdd5ee9c6
Christopher Kaster <ikasoki@gmail.com>
2014-10-21 19:35:23 +0200

    add copy to clipboard

--- printer.py
+++ printer.py
@@ -1,12 +1,17 @@
 from webcolors import hex_to_name
 
 def push_colors(handler, hex_query, rgb):
-    handler.add_new_item(title=hex_query, subtitle="Hex")
+    handler.add_new_item(title=hex_query, subtitle="Hex", arg=hex_query)
 
     try:
         name = hex_to_name(hex_query, spec='css3')
-        handler.add_new_item(title="color: %s;" % name, subtitle="CSS3")
+
+        css3_color = "color: %s;" % name
+
+        handler.add_new_item(title=css3_color, subtitle="CSS3", arg=css3_color)
     except ValueError:
         pass
 
-    handler.add_new_item(title="rgb(%s, %s, %s)" % rgb, subtitle="RGB")
+    rgb_str = "rgb(%s, %s, %s)" % rgb
+
+    handler.add_new_item(title=rgb_str, subtitle="RGB", arg=rgb_str)
